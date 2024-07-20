ee92e2c411883e39c412d5a94baa7c731b1272c6
PrateekSahu9808 <mailprateeksahu@gmail.com>
2024-07-21 00:07:23 +0530

    quizz app

---
+++ src/components/QuestionTimmer.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const QuestionTimmer = ({ timeout, onTimeOut,mode }) =>{
+  const[remainingTime,setRemainingTime]= useState(timeout)
+
+  useEffect(()=>{
+  const timer=  setTimeout(onTimeOut, timeout);
+  return ()=>{
+        clearTimeout(timer);
+      }
+  },[timeout,onTimeOut])
+  useEffect(()=>{
+  const interval=  setInterval(()=>{
+        setRemainingTime((prevTime)=>prevTime-100)
+      },100);
+      return ()=>{
+        clearInterval(interval);
+      }
+      
+  },[])
+
+  return <progress id="question-time" max={timeout} value={remainingTime} className={mode}/>;
+};
+
+export default QuestionTimmer;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from "react";
+import QUESTIONS from "../questions";
+
+import QuestionTimmer from "./QuestionTimmer";
+import Answers from "./Answers";
+import Question from "./Question";
+import Summary from "./Summary";
+const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(
+    (selectedAnswer) => {
+      
+      setUserAnswers((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+    []}
+  );
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+     <Summary userAnswers={userAnswers} />
+    );
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+};
+export default Quiz;
