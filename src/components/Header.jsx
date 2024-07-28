4908a8b8828e01cca4a83851adba7fab7b540a32
PrateekSahu9808 <mailprateeksahu@gmail.com>
2024-07-28 17:17:55 +0530

    food odering

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import logoImg from "../assets/logo.jpg"
+import Button from '../UI/Button'
+import CartContext from '../store/CartContext'
+import UserProgressContext from '../store/UserProgressContext'
+const Header = () => {
+  let cartCtx=useContext(CartContext)
+  let userProgressCtx=useContext(UserProgressContext)
+  const totalCartItems= cartCtx.items.reduce((totalNumberOfItems,item)=>{
+    return totalNumberOfItems+item.quantity
+  },0)
+
+  const handleShowCart=()=>{
+    userProgressCtx.showCart()
+  }
+  return (
+    <header id='main-header'>
+        <div id='title'>
+            <img src={logoImg} alt='A restaurant' />
+            <h1>Food Wala🍲</h1>
+        </div>
+        <nav>
+            <Button textOnly onClick={handleShowCart} > Cart ({totalCartItems})</Button>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+const sendHttpRequest = async (url, config) => {
+  const response = await fetch(url, config);
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error(
+      resData.message || "Something went wrong ,failed to send request 🍭"
+    );
+  }
+  return resData;
+};
+const useHttp = (url, config,initialData) => {
+  const [data, setData] = useState(initialData);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const clearData=()=>{
+    setData(initialData)
+  }
+  //this function is used to updating the some state based on response
+  const sendRequest = useCallback(async (data) => {
+    setIsLoading(true);
+    try {
+      const resData =await sendHttpRequest(url, {...config,body:data});
+      setData(resData);
+    } catch (error) {
+      setError(error.message || "Something went wrong");
+    }
+    setIsLoading(false);
+  }, [url, config]);
+  useEffect(() => {
+    if (config && (config.method === "GET" || !config.method)||!config) {
+        sendRequest();
+    }
+
+  }, [sendRequest,config]);
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest,
+    clearData
+  };
+};
+
+export default useHttp;
